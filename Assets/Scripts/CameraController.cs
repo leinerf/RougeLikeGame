c55c0bbde7a540d08fd9f22e128c7eb52d229c7f
Freniel <frenielzabala@gmail.com>
2019-11-07 15:59:59 -0800

    added walls and doors

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public static CameraController instance;
+
+    public float moveSpeed;
+
+    public Transform target;
+
+
+    private void Awake() 
+    {
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target != null)
+        {
+            Vector3 newPosition = new Vector3(
+                        target.position.x,
+                        target.position.y,
+                        transform.position.z
+                    );
+            transform.position = Vector3.MoveTowards(transform.position, newPosition, moveSpeed * Time.deltaTime);
+        }
+
+    }
+
+    public void ChangeTarget(Transform newTarget)
+    {
+        target = newTarget;
+    }
+}
---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+
+    public bool closedWhenEntered, openWhenEnemiesCleared;
+
+    public GameObject[] doors;//cant resize without work
+    public List<GameObject> enemies = new List<GameObject>();//resizable array
+
+    private bool roomActive = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (roomActive && openWhenEnemiesCleared && enemies.Count > 0)
+        {
+            for (int i = 0; i < enemies.Count; i++) 
+            {
+                if(enemies[i] == null)
+                {
+                    enemies.RemoveAt(i);
+                    i--;//for items removed the next item's index becomes the removed items index
+                }
+            }
+            if(enemies.Count == 0)
+            {
+                foreach (GameObject door in doors)
+                {
+                    door.SetActive(false);
+                }
+                closedWhenEntered = false;
+            }
+        }
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            CameraController.instance.ChangeTarget(transform);
+            roomActive = true;
+            if (closedWhenEntered)
+            {
+                foreach (GameObject door in doors)
+                {
+                    door.SetActive(true);
+                }
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            roomActive = false;
+        }
+    }
+}
+//on child object because we want to have the walls interact with bulletes
